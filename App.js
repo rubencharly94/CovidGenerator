7a4678868bbfd9e6eaaacac6b65a3b9484701443
rubencharly94 <rubencharly94@hotmail.com>
2021-10-22 17:44:30 +0100

    v1
    very basic structure

--- App.js
+++ App.js
@@ -1,14 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import { Button, StyleSheet, Text, View } from 'react-native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name = "StartScreen"
+          component ={HomeScreen}
+        />
+        <Stack.Screen
+          name="Private Gallery"
+          component={PrivateGallery}
+        />
+      
+      </Stack.Navigator>
+    </NavigationContainer>
   );
+
 }
 
 const styles = StyleSheet.create({
@@ -19,3 +33,31 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+const HomeScreen = ({navigation}) => {
+  return (
+    <View style={styles.container}>
+        <Text>Open up App.js to your app!</Text>
+        <Button 
+        title = "Access to Private Gallery"
+        onPress={() =>
+          navigation.navigate('Private Gallery')}
+        />
+
+        <StatusBar style="auto" />
+      </View>
+  )
+}
+
+const PrivateGallery = ({navigation}) => {
+  return (
+    <View style={styles.container}>
+        <Text>Here you can add images from your phone to the private Gallery</Text>
+        <Button 
+        title = "Add image"
+        />
+
+        <StatusBar style="auto" />
+      </View>
+  )
+}
